0a1f0c55b18b11503e715c89745ec47239769072
Dholakiya pratik milanbhai <158017664+pratikdholakiya0@users.noreply.github.com>
2026-01-08 21:41:12 +0530

    Update SecurityConfig.java

--- src/main/java/org/akashbag/ecommerce/config/SecurityConfig.java
+++ src/main/java/org/akashbag/ecommerce/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.akashbag.ecommerce.service.UserDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -30,7 +31,7 @@ public class SecurityConfig {
                     // auth requests
                     r.requestMatchers("/auth/**").permitAll()
                              .requestMatchers("/invoice/**").permitAll()
-                             .requestMatchers("/product/search").permitAll()
+                             .requestMatchers(HttpMethod.GET, "/product/**").permitAll()
                              .requestMatchers(
                                      "/api/cart/**",
                                      "/orders/cancel/",
